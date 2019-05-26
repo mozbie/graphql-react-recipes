2bc4efa53cbbb788cf45945c39ab71f6caef49b0
Alina Kizilova <alinakzilova@gmail.com>
2019-05-26 20:24:37 +0300

    preparation for deployment

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use(async (req, res, next) => {
 });
 
 // Create GraphiQL application
-app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+// app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
 
 // Connect schemas with GraphQL
 app.use(
@@ -74,13 +74,13 @@ app.use(
   }))
 );
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 4444;
 
